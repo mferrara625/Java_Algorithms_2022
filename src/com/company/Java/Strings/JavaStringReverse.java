44e5163d67e4f3e7f494a5d7541fcf471c7e50b4
mferrara625 <mferrara401@yahoo.com>
2022-09-11 23:51:48 -0400

    added 2 algorithms

---
+++ src/com/company/Java/Strings/JavaStringReverse.java
@@ -0,0 +1,22 @@
+package com.company.Java.Strings;
+
+import java.util.Scanner;
+
+public class JavaStringReverse {
+
+    public static void javaStringReverse() {
+        String result = "Yes";
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        for(int i = 0; i < A.length()/2; i++){
+            if(A.charAt(i) != A.charAt(A.length() - 1 - i)){
+                result = "No";
+                break;
+            }
+
+        }
+        System.out.println(result);
+
+    }
+}
