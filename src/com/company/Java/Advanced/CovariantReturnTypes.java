00a188a6b8cc118d80c163d5b55c4ebd551865c6
mferrara625 <mferrara401@yahoo.com>
2022-10-12 19:50:26 -0400

    CovariantReturnTypes

---
+++ src/com/company/Java/Advanced/CovariantReturnTypes.java
@@ -0,0 +1,46 @@
+package com.company.Java.Advanced;
+
+public class CovariantReturnTypes {
+
+//    https://www.hackerrank.com/challenges/java-covariance/problem
+
+    class Flower {
+        public String whatsYourName(){
+            return "I have many names and types.";
+        }
+    }
+
+    class Jasmine extends Flower {
+        @Override
+        public String whatsYourName(){
+            return "Jasmine";
+        }
+    }
+
+    class Lily extends Flower{
+        @Override
+        public String whatsYourName(){
+            return "Lily";
+        }
+    }
+
+    class Region {
+        public Flower yourNationalFlower(){
+            return new Flower();
+        }
+    }
+
+    class WestBengal extends Region{
+        @Override
+        public Jasmine yourNationalFlower(){
+            return new Jasmine();
+        }
+    }
+
+    class AndhraPradesh extends Region{
+        @Override
+        public Lily yourNationalFlower(){
+            return new Lily();
+        }
+    }
+}
