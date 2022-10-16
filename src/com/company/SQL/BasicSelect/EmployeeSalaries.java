b286322956066110d830ad144805d11c8f7968d2
mferrara625 <mferrara401@yahoo.com>
2022-10-16 17:38:12 -0400

    added EmployeeSalaries

---
+++ src/com/company/SQL/BasicSelect/EmployeeSalaries.java
@@ -0,0 +1,9 @@
+package com.company.SQL.BasicSelect;
+
+public class EmployeeSalaries {
+
+//    https://www.hackerrank.com/challenges/salary-of-employees/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+
+//    SELECT NAME FROM EMPLOYEE WHERE SALARY > 2000 AND MONTHS < 10 ORDER BY EMPLOYEE_ID ASC
+
+}
