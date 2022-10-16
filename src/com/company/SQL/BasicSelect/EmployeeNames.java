6274a7ef97e441cb8673c58a86fd72e09d983fe6
mferrara625 <mferrara401@yahoo.com>
2022-10-15 21:04:34 -0400

    added employeeNames to SQL

---
+++ src/com/company/SQL/BasicSelect/EmployeeNames.java
@@ -0,0 +1,8 @@
+package com.company.SQL.BasicSelect;
+
+public class EmployeeNames {
+
+//    https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=true
+
+//    SELECT NAME FROM EMPLOYEE ORDER BY NAME ASC;
+}
