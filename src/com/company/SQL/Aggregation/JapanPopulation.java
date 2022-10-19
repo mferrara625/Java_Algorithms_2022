703a06234094e2eb87d477b1578bab035d3efe90
mferrara625 <mferrara401@yahoo.com>
2022-10-19 16:00:31 -0400

    JapanPopulation

---
+++ src/com/company/SQL/Aggregation/JapanPopulation.java
@@ -0,0 +1,9 @@
+package com.company.SQL.Aggregation;
+
+public class JapanPopulation {
+
+//    https://www.hackerrank.com/challenges/japan-population/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+
+//    SELECT SUM(POPULATION) FROM CITY WHERE COUNTRYCODE = "JPN"
+
+}
