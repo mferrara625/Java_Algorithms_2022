60b87bdc361dc48fde77fc8ae8126f39e03e147d
mferrara625 <mferrara401@yahoo.com>
2022-10-23 12:01:43 -0400

    DesignerPDFViewer

---
+++ src/com/company/Algorithms/Implementation/DesignerPDFViewer.java
@@ -0,0 +1,17 @@
+package com.company.Algorithms.Implementation;
+
+import java.util.List;
+
+public class DesignerPDFViewer {
+
+//    https://www.hackerrank.com/challenges/designer-pdf-viewer/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+
+    public static int designerPdfViewer(List<Integer> h, String word) {
+        int tallest = 0;
+        for(char c : word.toCharArray()){
+            if(h.get(c - 97) > tallest)
+                tallest = h.get(c - 97);
+        }
+        return tallest * word.length();
+    }
+}
