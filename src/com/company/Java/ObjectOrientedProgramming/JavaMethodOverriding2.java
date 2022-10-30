4f5ad01356bdbdc15ec6f5aa6cd2c459fb1c1d49
mferrara625 <mferrara401@yahoo.com>
2022-10-30 19:37:47 -0400

    JavaMethodOverriding2

---
+++ src/com/company/Java/ObjectOrientedProgramming/JavaMethodOverriding2.java
@@ -0,0 +1,8 @@
+package com.company.Java.ObjectOrientedProgramming;
+
+public class JavaMethodOverriding2 {
+
+//    https://www.hackerrank.com/challenges/java-method-overriding-2-super-keyword/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+
+//    String temp= super.define_me();
+}
