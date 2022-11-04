5bf0e1547124de75a9fb1210bd2671fda66b05c5
mferrara625 <mferrara401@yahoo.com>
2022-11-04 18:58:29 -0400

    higher than 75 marks

---
+++ src/com/company/SQL/BasicSelect/HigherThan75Marks.java
@@ -0,0 +1,9 @@
+package com.company.SQL.BasicSelect;
+
+public class HigherThan75Marks {
+
+//    https://www.hackerrank.com/challenges/more-than-75-marks/problem?isFullScreen=true
+
+//    SELECT NAME FROM STUDENTS WHERE MARKS > 75 ORDER BY RIGHT(NAME,3), ID ASC;
+
+}
