3b9687d91796228659a127523d9c2fb8d2730158
mferrara625 <mferrara401@yahoo.com>
2022-11-09 18:13:15 -0500

    added RevisingAggregations_TheCountFunction

---
+++ src/com/company/SQL/Aggregation/RevisingAggregations_TheCountFunction.java
@@ -0,0 +1,10 @@
+package com.company.SQL.Aggregation;
+
+public class RevisingAggregations_TheCountFunction {
+
+//    https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem?isFullScreen=true
+
+//    SELECT COUNT(NAME) FROM CITY WHERE POPULATION > 100000;
+
+//    NOTE: 'NAME' in this query can be replaced with any column in CITY;
+}
