98656b758e3c6fa4956dda5cddfb25023612eabf
mferrara625 <mferrara401@yahoo.com>
2022-11-10 16:20:54 -0500

    R/A_TheSumFunction

---
+++ src/com/company/SQL/Aggregation/RevisingAggregations_TheSumFunction.java
@@ -0,0 +1,9 @@
+package com.company.SQL.Aggregation;
+
+public class RevisingAggregations_TheSumFunction {
+
+//    https://www.hackerrank.com/challenges/revising-aggregations-sum/problem?isFullScreen=true
+
+//    SELECT SUM(POPULATION) FROM CITY WHERE DISTRICT = 'California';
+
+}
