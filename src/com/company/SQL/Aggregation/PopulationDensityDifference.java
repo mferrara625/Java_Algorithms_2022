dc516980f6818b962be9ee1b4a865a6a858f40a7
mferrara625 <mferrara401@yahoo.com>
2022-11-11 14:39:24 -0500

    PopulationDensityDifference

---
+++ src/com/company/SQL/Aggregation/PopulationDensityDifference.java
@@ -0,0 +1,8 @@
+package com.company.SQL.Aggregation;
+
+public class PopulationDensityDifference {
+
+//    https://www.hackerrank.com/challenges/population-density-difference/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+//    SELECT MAX(POPULATION) - MIN(POPULATION) FROM CITY;
+}
