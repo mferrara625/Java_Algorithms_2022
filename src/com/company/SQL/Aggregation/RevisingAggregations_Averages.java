13a9c64dc0cb4beb005e1fc9ea5a040170b0ebb3
mferrara625 <mferrara401@yahoo.com>
2022-11-11 14:37:18 -0500

    RA_Averages

---
+++ src/com/company/SQL/Aggregation/RevisingAggregations_Averages.java
@@ -0,0 +1,8 @@
+package com.company.SQL.Aggregation;
+
+public class RevisingAggregations_Averages {
+
+//    https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+
+//    SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT = 'California';
+}
