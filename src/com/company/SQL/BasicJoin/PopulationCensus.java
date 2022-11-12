dcf32dedc23ea48605ed1f265efb5e3538e3bba3
mferrara625 <mferrara401@yahoo.com>
2022-11-12 12:58:47 -0500

    PopulationCensus

---
+++ src/com/company/SQL/BasicJoin/PopulationCensus.java
@@ -0,0 +1,8 @@
+package com.company.SQL.BasicJoin;
+
+public class PopulationCensus {
+
+//    https://www.hackerrank.com/challenges/asian-population/problem?isFullScreen=true
+
+//    SELECT SUM(CITY.POPULATION) FROM CITY JOIN COUNTRY ON CITY.COUNTRYCODE=COUNTRY.CODE WHERE CONTINENT = "Asia";
+}
