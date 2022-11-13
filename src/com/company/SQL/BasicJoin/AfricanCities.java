3a5baf184b4f3ecbc5211af8ec7dcfa2185ea175
mferrara625 <mferrara401@yahoo.com>
2022-11-13 12:51:21 -0500

    AfricanCities

---
+++ src/com/company/SQL/BasicJoin/AfricanCities.java
@@ -0,0 +1,8 @@
+package com.company.SQL.BasicJoin;
+
+public class AfricanCities {
+
+//    https://www.hackerrank.com/challenges/african-cities/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+
+//    SELECT CITY.NAME FROM CITY JOIN COUNTRY ON CITY.COUNTRYCODE = COUNTRY.CODE WHERE COUNTRY.CONTINENT = 'Africa';
+}
