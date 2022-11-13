5ef420a99c07876ca5b9a223211ae196fc1dfb54
mferrara625 <mferrara401@yahoo.com>
2022-11-13 13:56:55 -0500

    AveragePopulationOfEachContinent

---
+++ src/com/company/SQL/BasicJoin/AveragePopulationOfEachContinent.java
@@ -0,0 +1,8 @@
+package com.company.SQL.BasicJoin;
+
+public class AveragePopulationOfEachContinent {
+
+//    https://www.hackerrank.com/challenges/average-population-of-each-continent/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+//    SELECT COUNTRY.CONTINENT, FLOOR(AVG(CITY.POPULATION)) FROM CITY JOIN COUNTRY ON CITY.COUNTRYCODE = COUNTRY.CODE GROUP BY COUNTRY.CONTINENT;
+}
