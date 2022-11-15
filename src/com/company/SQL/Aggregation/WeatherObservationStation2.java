b04afac16cb196b49bef3e9bd5b177ac1adc7b12
mferrara625 <mferrara401@yahoo.com>
2022-11-15 17:32:33 -0500

    WeatherObservationStation2

---
+++ src/com/company/SQL/Aggregation/WeatherObservationStation2.java
@@ -0,0 +1,8 @@
+package com.company.SQL.Aggregation;
+
+public class WeatherObservationStation2 {
+
+//    https://www.hackerrank.com/challenges/weather-observation-station-2/problem
+
+//    SELECT CAST(SUM(LAT_N) AS DECIMAL(10, 2)), CAST(SUM(LONG_W) AS DECIMAL(10, 2)) FROM STATION;
+}
