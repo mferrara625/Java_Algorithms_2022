570367d7cd039010fc3b9e8c9374fc2a8746e701
mferrara625 <mferrara401@yahoo.com>
2022-11-14 21:14:25 -0500

    TopEarners

---
+++ src/com/company/SQL/Aggregation/TopEarners.java
@@ -0,0 +1,8 @@
+package com.company.SQL.Aggregation;
+
+public class TopEarners {
+
+//    https://www.hackerrank.com/challenges/earnings-of-employees/problem?isFullScreen=true
+
+//    SELECT MONTHS*SALARY, COUNT(*) FROM EMPLOYEE GROUP BY 1 ORDER BY 1 DESC LIMIT 1;
+}
