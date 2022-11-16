cef46b62435f2dd9bd7b33c4ac2bca2c03d24325
mferrara625 <mferrara401@yahoo.com>
2022-11-16 15:15:27 -0500

    WeatherObservationStation13

---
+++ src/com/company/SQL/Aggregation/WeatherObservationStation13.java
@@ -0,0 +1,8 @@
+package com.company.SQL.Aggregation;
+
+public class WeatherObservationStation13 {
+
+//    https://www.hackerrank.com/challenges/weather-observation-station-13/problem?h_r=next-challenge&h_v=zen
+
+//    SELECT CAST(SUM(LAT_N) AS DECIMAL(10, 4)) FROM STATION WHERE LAT_N > 38.7880 AND LAT_N < 137.2345;
+}
