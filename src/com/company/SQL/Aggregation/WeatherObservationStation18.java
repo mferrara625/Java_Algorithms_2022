4724b6891ae6942c1eec506b75d2308bfffe0d37
mferrara625 <mferrara401@yahoo.com>
2022-11-22 12:56:38 -0500

    WeatherObservationStation18

---
+++ src/com/company/SQL/Aggregation/WeatherObservationStation18.java
@@ -0,0 +1,8 @@
+package com.company.SQL.Aggregation;
+
+public class WeatherObservationStation18 {
+
+//    https://www.hackerrank.com/challenges/weather-observation-station-18/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+//    SELECT CAST(ABS((MIN(LAT_N) - MAX(LAT_N))) + ABS((MIN(LONG_W) - MAX(LONG_W))) AS DECIMAL(10, 4)) FROM STATION;
+}
