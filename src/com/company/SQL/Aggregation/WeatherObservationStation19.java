9332aeb8213b10c2451b4ab8051df8e895d41b2f
mferrara625 <mferrara401@yahoo.com>
2022-11-23 15:48:28 -0500

    WeatherObservationStation19

---
+++ src/com/company/SQL/Aggregation/WeatherObservationStation19.java
@@ -0,0 +1,8 @@
+package com.company.SQL.Aggregation;
+
+public class WeatherObservationStation19 {
+
+//    https://www.hackerrank.com/challenges/weather-observation-station-19/problem?isFullScreen=true
+
+//    SELECT CAST(SQRT(POWER((MAX(LAT_N) - MIN(LAT_N)), 2) + POWER((MAX(LONG_W) - MIN(LONG_W)), 2)) AS DECIMAL(7,4)) FROM STATION;
+}
