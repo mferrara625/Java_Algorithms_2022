4bc70cf1388e24899af5bcca7673ea82b0f6331b
mferrara625 <mferrara401@yahoo.com>
2022-12-13 18:13:58 -0500

    TheBlunder

---
+++ src/com/company/SQL/Aggregation/TheBlunder.java
@@ -0,0 +1,8 @@
+package com.company.SQL.Aggregation;
+
+public class TheBlunder {
+
+//    https://www.hackerrank.com/challenges/the-blunder/problem
+
+//    SELECT CEIL(AVG(Salary) - AVG(REPLACE(Salary, '0', ''))) FROM EMPLOYEES;
+}
