163f51751b4bf082a920080cb2163bb0e0540fd8
mferrara625 <mferrara401@yahoo.com>
2022-12-14 20:44:31 -0500

    JavaCurrencyFormatter

---
+++ src/com/company/Java/Introduction/JavaCurrencyFormatter.java
@@ -0,0 +1,33 @@
+package com.company.Java.Introduction;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class JavaCurrencyFormatter {
+
+//    https://www.hackerrank.com/challenges/java-currency-formatter/problem
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        Locale locale = new Locale("en", "in");
+
+        NumberFormat usFormat = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat indiaFormat = NumberFormat.getCurrencyInstance(locale);
+        NumberFormat chinaFormat = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat franceFormat = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+
+        String us = usFormat.format(payment);
+        String india = indiaFormat.format(payment);
+        String china = chinaFormat.format(payment);
+        String france = franceFormat.format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
