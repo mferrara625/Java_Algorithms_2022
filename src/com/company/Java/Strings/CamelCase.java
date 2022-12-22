e3b5587f0ceda3801263902db4314e146383f3df
mferrara625 <mferrara401@yahoo.com>
2022-12-22 16:22:30 -0500

    CamelCase

---
+++ src/com/company/Java/Strings/CamelCase.java
@@ -0,0 +1,18 @@
+package com.company.Java.Strings;
+
+public class CamelCase {
+
+//    https://www.hackerrank.com/challenges/camelcase/problem?isFullScreen=true
+
+    public static int camelcase(String s) {
+        // Write your code here
+
+        int result = 1;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) <= 'Z' && s.charAt(i) >= 'A'){
+                result++;
+            }
+        }
+        return result;
+    }
+}
