867e07df4ff6958b77df26c117ffcbb04b613f3e
mferrara625 <mferrara401@yahoo.com>
2022-12-25 10:06:43 -0500

    Placements

---
+++ src/com/company/SQL/AdvancedJoin/Placements.java
@@ -0,0 +1,8 @@
+package com.company.SQL.AdvancedJoin;
+
+public class Placements {
+
+//    https://www.hackerrank.com/challenges/placements/problem?isFullScreen=true
+
+//    SELECT STUDENTS.NAME FROM STUDENTS INNER JOIN FRIENDS ON STUDENTS.ID = FRIENDS.ID INNER JOIN PACKAGES ON FRIENDS.FRIEND_ID = PACKAGES.ID WHERE PACKAGES.SALARY > (SELECT SALARY FROM PACKAGES WHERE ID = STUDENTS.ID) ORDER BY PACKAGES.SALARY ASC;
+}
