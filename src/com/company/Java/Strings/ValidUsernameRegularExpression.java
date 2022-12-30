30cdc94cf1d840f33391ae8632af1f811ddb65ed
mferrara625 <mferrara401@yahoo.com>
2022-12-29 19:58:07 -0500

    ValidUsernameRegularExpression

---
+++ src/com/company/Java/Strings/ValidUsernameRegularExpression.java
@@ -0,0 +1,8 @@
+package com.company.Java.Strings;
+
+public class ValidUsernameRegularExpression {
+
+//    https://www.hackerrank.com/challenges/valid-username-checker/problem?isFullScreen=true
+
+    public static final String regularExpression = "^[a-zA-Z]\\w{7,29}";
+}
