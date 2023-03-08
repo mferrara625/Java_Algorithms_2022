dedad57ff4b6a867f17cf9c924ade71d9e341c4c
mferrara625 <mferrara401@yahoo.com>
2023-03-07 21:14:22 -0500

    JavaRegex

---
+++ src/com/company/Java/Strings/JavaRegex.java
@@ -0,0 +1,12 @@
+package com.company.Java.Strings;
+
+public class JavaRegex {
+
+//    https://www.hackerrank.com/challenges/java-regex/problem?isFullScreen=true
+
+        final String pattern = "^([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\\." +
+                "([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\\." +
+                "([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\\." +
+                "([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])$";
+
+}
